d45e3ee49580d702fcfb905c5fa04681b235fc51
maxsimt <maxsimtorrub@gmail.com>
2026-05-30 18:11:18 +0300

    feat: create EventRepository

---
+++ backend/src/main/java/com/dispatcher/backend/repository/EventRepository.java
@@ -0,0 +1,16 @@
+package com.dispatcher.backend.repository;
+
+import com.dispatcher.backend.entity.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, UUID> {
+  List<Event> findByVehicleId(UUID vehicleId);
+
+  List<Event> findByStatus(String status);
+
+  List<Event> findByPriority(String priority);
+}
